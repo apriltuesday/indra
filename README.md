f19cf55f60de8bc551df493ec6ea36a0de1e4a40
April Shen <april.tuesday@gmail.com>
2019-12-21 15:23:32 +0000

    generating kissing schottky groups, and adding images

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # indra
 
+![necklace](images/necklace-zoom.png)
+
 Projects from the book [Indra's Pearls](http://klein.math.okstate.edu/IndrasPearls/).
--- indra/common.py
+++ indra/common.py
@@ -4,4 +4,5 @@ from collections import namedtuple
 # center is complex, radius is real
 Circle = namedtuple('Circle', ['center', 'radius'])
 
-EPS = 1e-12
+NUMERIC_EPS = 1e-12
+VISUAL_EPS = 1e-4
